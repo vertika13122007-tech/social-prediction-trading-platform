c29820dea1a388ad4a457fdac6a98d3bc10771d3
vertika13122007-tech <vertika13122007@gmail.com>
2026-08-04 19:08:57 +0530

    Connect front-end to production backend

--- front-end/src/api/axios.js
+++ front-end/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL:"https://predictx-social-prediction-trading.onrender.com/api",
     headers:{
         "Content-Type":"application/json"
     }
--- front-end/src/socket/socket.js
+++ front-end/src/socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io("https://predictx-social-prediction-trading.onrender.com", {
     withCredentials: true,
 });
